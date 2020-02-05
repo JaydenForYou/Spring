bfe41f67679676eb1a2c11e7cb5368a6e9b28624
codewhy <i@iobiji.com>
2020-02-05 16:32:42 +0800

    Update header.php

--- header.php
+++ header.php
@@ -12,10 +12,10 @@ if (!empty($this->options->cdn) && $this->options->cdn) {
   <meta name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no"/>
   <title><?php $this->archiveTitle(array(
-      'category' => _t('分类 %s 下的文章'),
-      'search' => _t('包含关键字 %s 的文章'),
-      'tag' => _t('标签 %s 下的文章'),
-      'author' => _t('%s 发布的文章')
+      'category' => _t('%s'),
+      'search' => _t('%s'),
+      'tag' => _t('%s'),
+      'author' => _t('%s')
     ), '', ' - '); ?><?php $this->options->title(); ?><?php if ($this->is('index') && $this->options->Subtitle != null) {
       echo ' - ' . $this->options->Subtitle;
     } ?></title>
