010c4f21968ffffc13bef6341c573001bc70212e
codewhy <i@iobiji.com>
2020-02-17 18:07:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,11 @@ $this->need('header.php');
 $hiddens = '';
 $hidden = '';
 $page = '';
-$page = preg_replace("/\/\d+/u",'',$_SERVER['PHP_SELF']);
+if ($this->is('index')) {
+  $page = "/index.php/page/";
+} else {
+  $page = preg_replace("/\/\d+/u",'',$_SERVER['PHP_SELF']);
+}
 $prev = $this->_currentPage - 1;
 $next = $this->_currentPage + 1;
 if ($this->_currentPage == 0 || $this->_currentPage == 1) {
