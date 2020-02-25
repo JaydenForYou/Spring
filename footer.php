3e06f62e32e79b0e286d1a2b228076f540e45519
codewhy <i@iobiji.com>
2020-02-26 00:00:06 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -42,7 +42,7 @@ if (!empty($this->options->cdn) && $this->options->cdn) {
 </div>
 </div>
 
-<div class="animated fixed-to-top click-to-top">
+<div class="d-flex justify-content-center align-items-center flex-column animated fixed-to-top click-to-top">
   <i class="fas fa-angle-double-up"></i>
   <?php if ($this->is('post') || $this->is('page')): ?>
     <span class="animated progress-number"></span>
