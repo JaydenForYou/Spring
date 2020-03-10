99ac91dadf4f464b9711fcbea109fcbde39fe550
codewhy <i@iobiji.com>
2020-03-11 02:49:00 +0800

    修复无法评论问题

--- comments.php
+++ comments.php
@@ -117,6 +117,7 @@ function threadedComments($comments, $options)
       </div>
     <?php endif; ?>
   </form>
+  <?php if ($this->options->commentsAntiSpam): ?><nocompress><script>(function(){var a=document.addEventListener?{add:"addEventListener",focus:"focus",load:"DOMContentLoaded"}:{add:"attachEvent",focus:"onfocus",load:"onload"};var c,d,e,f,b=document.getElementById("<?php $this->respondId() ?>");null!=b&&(c=b.getElementsByTagName("form"),c.length>0&&(d=c[0],e=d.getElementsByTagName("textarea")[0],f=!1,null!=e&&"text"==e.name&&e[a.add](a.focus,function(){if(!f){var a=document.createElement("input");a.type="hidden",a.name="_",d.appendChild(a),f=!0,a.value=<?php echo Typecho_Common::shuffleScriptVar($this->security->getToken(preg_replace('/[\?|\&]_pjax=[^&]+/i','',$this->request->getRequestUrl()))) ?>}})))})();</script></nocompress><?php endif ?>
   <?php if ($this->commentsNum != 0): ?>
     <div class="vinfo" style="display:block;">
       <div class="vcount col">
