3a19a2eabfa5b9d13157867efd1f7a8c9566b9ba
codewhy <i@iobiji.com>
2020-03-16 18:54:56 +0800

    优化布局

--- page-about.php
+++ page-about.php
@@ -54,4 +54,17 @@ $this->need('header.php');
     </div>
   </div>
 </main>
+<section class="main-footer-waves-area waves-area">
+  <svg class="waves-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+    <defs>
+      <path id="gentle-wave" d="M -160 44 c 30 0 58 -18 88 -18 s 58 18 88 18 s 58 -18 88 -18 s 58 18 88 18 v 44 h -352 Z" />
+    </defs>
+    <g class="parallax">
+      <use xlink:href="#gentle-wave" x="48" y="0" />
+      <use xlink:href="#gentle-wave" x="48" y="3" />
+      <use xlink:href="#gentle-wave" x="48" y="5" />
+      <use xlink:href="#gentle-wave" x="48" y="7" />
+    </g>
+  </svg>
+</section>
 <?php $this->need('footer.php'); ?>
