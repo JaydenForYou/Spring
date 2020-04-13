19de33b031a20aebd3cdea7cba1e4815f6567f19
codewhy <admin@iobiji.com>
2020-04-13 22:32:46 +0800

    Update page-about.php

--- page-about.php
+++ page-about.php
@@ -17,7 +17,7 @@ $this->need('header.php');
     <div class="text-center main-hero-content-description"><?php $this->author(); ?>
       / <?= date('Y-m-d', $this->created) ?></div>
   </div>
-  <div class="main-hero-header"></div>
+  <div class="main-hero-header header-right"></div>
 </section>
 <main class="main-content custom-about-template">
   <div class="d-flex justify-content-center align-items-center flex-column about-me-home">
