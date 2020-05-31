b2f2284b4bee9e9e5ea48d96df9f463fc05299f5
codewhy <admin@iobiji.com>
2020-05-31 14:29:35 +0800

    Update page-archive.php

--- page-archive.php
+++ page-archive.php
@@ -12,26 +12,26 @@ $this->need('header.php');
 <?php Typecho_Widget::widget('Widget_Stat')->to($stat); ?>
   <body class="page-template page-links spring-body">
   <section class="main-hero">
-    <div class="main-hero-bg"
-         style="background-image: url('<?= $this->fields->thumbnail ?>')"></div>
-    <div class="d-flex flex-column align-content-center justify-content-center main-hero-content">
-      <div class="text-center main-hero-content-title"><?= $this->title; ?></div>
-      <div class="main-hero-waves-area waves-area">
-        <svg class="waves-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-             viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
-          <defs>
-            <path id="gentle-wave"
-                  d="M -160 44 c 30 0 58 -18 88 -18 s 58 18 88 18 s 58 -18 88 -18 s 58 18 88 18 v 44 h -352 Z"/>
-          </defs>
-          <g class="parallax">
-            <use xlink:href="#gentle-wave" x="48" y="0"/>
-            <use xlink:href="#gentle-wave" x="48" y="3"/>
-            <use xlink:href="#gentle-wave" x="48" y="5"/>
-            <use xlink:href="#gentle-wave" x="48" y="7"/>
-          </g>
-        </svg>
-      </div>
-  </section>
+  <div class="main-hero-bg"
+       style="background-image: url('<?= $this->fields->thumbnail ?>')"></div>
+  <div class="d-flex flex-column align-content-center justify-content-center main-hero-content">
+    <div class="text-center main-hero-content-title"><?= $this->title; ?></div>
+  <div class="main-hero-waves-area waves-area">
+    <svg class="waves-svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+         viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+      <defs>
+        <path id="gentle-wave"
+              d="M -160 44 c 30 0 58 -18 88 -18 s 58 18 88 18 s 58 -18 88 -18 s 58 18 88 18 v 44 h -352 Z"/>
+      </defs>
+      <g class="parallax">
+        <use xlink:href="#gentle-wave" x="48" y="0"/>
+        <use xlink:href="#gentle-wave" x="48" y="3"/>
+        <use xlink:href="#gentle-wave" x="48" y="5"/>
+        <use xlink:href="#gentle-wave" x="48" y="7"/>
+      </g>
+    </svg>
+  </div>
+</section>
   <main class="main-content">
     <div class="container-sm">
       <div class="row post-content-main">
